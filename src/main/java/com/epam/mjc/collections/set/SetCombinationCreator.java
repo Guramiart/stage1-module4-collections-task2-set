c319e64161e8aca3b362459cf0e799a08a88fb3e
Artem Pasko <guramibox777@gmail.com>
2022-08-20 23:18:19 +0300

    refactored code

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -4,26 +4,18 @@ import java.util.*;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-        Set<String> firstCopySet = new HashSet<>(firstSet);
-        Set<String> secondCopySet = new HashSet<>(secondSet);
-        Set<String> thirdCopySet = new HashSet<>(thirdSet);
-
         Set<String> firstAndSecondDuplicateSet = new HashSet<>();
-        for(String element : new ArrayList<>(firstCopySet)) {
-            if(!secondCopySet.add(element)) {
+        for(String element : new ArrayList<>(firstSet)) {
+            if(secondSet.contains(element)) {
                 firstAndSecondDuplicateSet.add(element);
-            } else {
-                secondCopySet.remove(element);
             }
         }
 
         Set<String> thirdUniqueSet = new HashSet<>();
-        for(String element : new ArrayList<>(thirdCopySet)) {
-            if(firstCopySet.add(element) && secondCopySet.add(element)) {
+        for(String element : new ArrayList<>(thirdSet)) {
+            if(!firstSet.contains(element) && !secondSet.contains(element)) {
                 thirdUniqueSet.add(element);
             }
-            firstCopySet.remove(element);
-            secondCopySet.remove(element);
         }
 
         thirdUniqueSet.addAll(firstAndSecondDuplicateSet);
